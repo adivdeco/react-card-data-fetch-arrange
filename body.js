76d5436313b9ee97905af96af189081154b03c06
adivdeco <sadiv120@gmail.com>
2025-02-01 22:45:23 +0530

    filter by name

--- body.js
+++ body.js
@@ -6,14 +6,21 @@ function Body() {
      
     const [users, setUsers] = useState([]);
     const [num, setnum] = useState('');
+    const [txt, settxt] = useState('');
     
 
-    async function fetchdata(count) {
+    async function fetchdata(count,name = "") {
         const random = Math.floor(Math.random() * 1000);
-        const responce = await fetch(`https://api.github.com/users?since=${random}&per_page=${count}`);
+        const responce = await fetch(`https://api.github.com/users${name}since=${random}&per_page=${count}`);
         const data = await responce.json();
         console.log(data);
-        setUsers(data);
+        setUsers(Array.isArray(data)? data : []);
+    }
+    async function fetchByName(name) {
+        const response = await fetch(`https://api.github.com/search/users?q=${name}`);
+        const data = await response.json();
+        console.log(data.items);
+        setUsers(Array.isArray(data.items) ? data.items : []);
     }
 
     useEffect(() => {
@@ -24,6 +31,11 @@ function Body() {
         <>
                    <input type="text" value={num} placeholder='no. of profiles needed' onChange={(e)=>setnum(e.target.value)}/>
                    <button onClick={()=>fetchdata(Number(num))}>search</button>
+
+                   <input type="text" value={txt} placeholder='search by name..' onChange={(e)=>settxt(e.target.value)}/>
+                   {/* <button onClick={()=>fetchdata( `login=${txt}`)}>search</button> */}
+                   {/* <button onClick={()=> users.filter((user)=>user.login.includes(txt))}>search</button> */}
+                   <button onClick={()=>fetchByName(txt)}>search</button>
 
         <div className="container">
             {
